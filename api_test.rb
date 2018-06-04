3a9e3e876d64cc5daf8c3ba97e12f2f85a507c22
Nicholas Utikal <nicholas.utikal@makerist.de>
2018-06-04 08:21:45 +0200

    finished smallRuby on Railsapplication providing an endpoint which takes a GPS latitude and longitude and spits out names of museums around that location

--- api_test.rb
+++ api_test.rb
@@ -9,9 +9,12 @@ latResponse = gets.chomp
 url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?&proximity=#{longResponse},#{latResponse}&
   limit=100&types=poi&access_token=pk.eyJ1IjoidGtoa29rZCIsImEiOiJjamh3Mng2dmEwYndoM3FtZHpyNT
     lxZ2w4In0.0cFFf-1oVAX_KSkuoAgzxg"
+
 latlong = open(url).read
 museum = JSON.parse(latlong)
+
 museumHash = Hash.new {|hsh, key| hsh[key] = []}
+
 n = museum["features"].length - 1
 for i in 0..n
   t = museum["features"][i]
@@ -21,6 +24,7 @@ for i in 0..n
   possibleZip1.count("a-zA-Z") > 0 ? museumHash[possibleZip2] << placeName :
     museumHash[possibleZip1] << placeName
 end
+
 puts museumHash
 
 
